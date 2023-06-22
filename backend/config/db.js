75f2e456cb80b18cfc29f6bba5db330c9abd1693
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-06-22 16:47:36 +0530

    g

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async  ()=>{
     try {
      const conn =  await  mongoose.connect(process.env.MONGO_URI)
-
+     console.log(`connected to mongoDb ${conn.connection.host}`);
+<<<<<<< HEAD
         console.log(`connected to mongoDb ${conn.connection.host}`);
+=======
+        
+>>>>>>> 9fd3272 (ec2 hosting)
     } catch (error) {
         process.exit(1)
     }
