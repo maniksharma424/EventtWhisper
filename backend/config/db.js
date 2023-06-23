1c5bec1432bc21904c894cc4f1754cd9e77bd56b
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-06-23 12:47:59 +0530

    protect event routes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,7 @@ const connectDB = async  ()=>{
     try {
      const conn =  await  mongoose.connect(process.env.MONGO_URI)
      console.log(`connected to mongoDb ${conn.connection.host}`);
-<<<<<<< HEAD
-        console.log(`connected to mongoDb ${conn.connection.host}`);
-=======
-        
->>>>>>> 9fd3272 (ec2 hosting)
+    
     } catch (error) {
         process.exit(1)
     }
