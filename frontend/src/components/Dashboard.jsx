145eacbc48cab7f610df4c3b2b5215492f2871ce
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-06-25 15:36:25 +0530

    protected routes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -48,7 +48,7 @@ throw new Error(error)
     }
   };
   useEffect(() => {
-    const socket = io("http://localhost:5000");
+    const socket = io("http://www.eventwhisper.click");
 
     socket.on("roomId", (roomId) => {
       console.log(`connected to room ${roomId}`);
