4f0b349f84fa8169fbf2c1c8d67f8230263af92e
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2023-06-25 22:27:13 +0530

    socket.io

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,8 @@ import { errorHandler, notFound } from "./middleware/errorMiddleWare.js";
 import connectDB from "./config/db.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import { Server } from "socket.io";
-import https from "https";
-import fs from "fs";
-import path from "path";
 
+import path from "path"
 dotenv.config();
 connectDB();
 const port = process.env.PORT || 5000;
@@ -17,7 +15,9 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
@@ -36,24 +36,19 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(errorHandler);
+
 app.use(notFound);
 
-const privateKeyPath = path.resolve("/etc/letsencrypt/live/eventwhisper.click/privkey.pem");
-const certificatePath = path.resolve("/etc/letsencrypt/live/eventwhisper.click/fullchain.pem");
+const server = app.listen(port, () =>
+  console.log(`server is running at port ${port}`)
+);
 
-const privateKey = fs.readFileSync(privateKeyPath, "utf8");
-const certificate = fs.readFileSync(certificatePath, "utf8");
-
-const credentials = { key: privateKey, cert: certificate };
-
-const server = https.createServer(credentials, app);
-
+export let roomId
 export const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
   },
 });
-export let roomId
 
 io.on("connection", (socket) => {
    roomId = socket.id; // Use socket ID as the room ID
@@ -61,13 +56,8 @@ io.on("connection", (socket) => {
   socket.join(roomId);
 
   socket.on("disconnect", () => {
-    // Handle disconnect logic
-  });
 
+  });
   // Emit an event to the client
   socket.emit("roomId", roomId);
 });
-
-server.listen(port, () =>
-  console.log(`server is running at port ${port}`)
-);
